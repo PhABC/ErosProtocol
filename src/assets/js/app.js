2e016b85bab557cbf39fbfac5172039e223b9295
Nuno Lopes <nuno.lopes@lopezi.com>
2017-10-14 19:35:32 +0200

    Multiple changes and clean up.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,6 +1,35 @@
 $(document).foundation();
 
 
+function submitBuy() {
+    var obj = {
+        amountBuy: $("amountBuy"),
+        price: $("price"),
+        ttl: $("ttl"),
+        feeBuy: $("feeBuy"),
+    }
+}
 
+function submitSell() {
+    var obj = {
+        amountSell: $("amountSell"),
+        pricePay: $("pricePay"),
+        ttl2: $("ttl2"),
+        feeSell: $("feeSell"),
+    }
+}
 
+function submit(type) {
+    if(!web3) throw new Error("no web3");
+    var obj = {test:"test2"}
+    var jsonObj = JSON.stringify(obj);
+    web3.util.toHex(JSON.stringify(obj));
+}
 
+function init() {
+    document.getElementById('broadcastBuy').onsubmit = submitBuy;
+    document.getElementById('broadcastSell').onsubmit = submitSell;
+}
+
+windows.onload = submitBuy;
+windows.onload = submitSell;
